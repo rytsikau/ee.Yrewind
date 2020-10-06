2635c2a601dd718cc4302989ab1f90f5956b122b
Eugen Rytikov <y.rytsikau@gmail.com>
2020-10-06 12:40:28 +0300

    Add files via upload

---
+++ start.bat
@@ -0,0 +1,5 @@
+yrewind -url="https://www.youtube.com/watch?v=9Auq9mYxFEE"
+yrewind -url="https://www.youtu.be/9Auq9mYxFEE" -resolution=144
+yrewind -url=9Auq9mYxFEE -start=20200715:0710 -pathchrome="c:\Program Files (x86)\Google\Chrome\"
+yrewind -url=9Auq9mYxFEE -start=Y:2355 -duration=10 -resolution=480
+pause
